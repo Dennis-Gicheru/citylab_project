04c48049decb56703e4977fbe78afe50fb991145
Dennis-Gicheru <dennisgicheru254@gmail.com>
2025-12-30 07:24:28 +0000

    Added rviz config and adapted requisite files

--- robot_patrol/launch/start_patrolling.launch.py
+++ robot_patrol/launch/start_patrolling.launch.py
@@ -1,7 +1,13 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    
+    pkg_share = get_package_share_directory('robot_patrol')
+    rviz_config_path = os.path.join(pkg_share, 'rviz', 'patrol_config.rviz')
+    
     return LaunchDescription([
         Node(
             package='robot_patrol',
@@ -11,4 +17,12 @@ def generate_launch_description():
         ('/cmd_vel', '/fastbot_1/cmd_vel'), ('/scan', '/fastbot_1/scan')
             ]
         ),
+
+       Node(
+           package='rviz2',
+           executable='rviz2',
+           name='rviz2',
+           arguments=['-d', rviz_config_path],
+           output='screen'
+       )
     ])
